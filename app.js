b56df6642390637dff10323ce0bf1b60b51ca6e0
Nghi <thanhnghi94@gmail.com>
2016-05-10 21:53:06 +0200

    Some minor changes

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 var express = require("express"),
-    connect = require("node-mysql"),
-    path = require("path"),
     morgan = require("morgan"),
     bodyParser = require("body-parser"),
-    sha1 = require("sha1"),
-    mysql = require("mysql");
+    signup = require("./routes/signup"),
+    login = require("./routes/login");
 
-// connect to database section
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "logInOut"
-});
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to mysql database! " + connection.threadId);
-});
 
 //middle ware section
 var app = express();
@@ -26,58 +13,55 @@ app.set("view engine", "ejs");
 // app.VERB section
 
 /* SIGNUP */
-app.get("/", function (req, res) {
-    res.render("signup");
-});
-
-app.post("/signup", function (req, res, next) {
-    // get the form submit file
-    var post = {
-        username: req.body.username,
-        email: req.body.email,
-        password: sha1(req.body.password)
-    };
-
-    // insert to user table
-    connection.query("INSERT INTO users SET ?", post,function (err, result) {
-        if (err) throw err;
-        res.redirect("/user");
-    });
-});
-
+app.use("/", signup);
 /* LOGIN */
-app.get("/login", function (req, res) {
-    res.render("login");
-});
-
-
-app.post("/login", function (req, res) {
-    // get user input
-    var post = {
-        username: req.body.username,
-        password: sha1(req.body.password)
-    };
-
-    // get from database
-    var query = "SELECT * FROM `users` WHERE `username` = '" + post.username + "' AND `password` = '" + post.password + "'";
+app.use("/login", login);
+/* UPDATE */
+/*
+app.get("/update/:userId", function (req, res) {
+    var query = "SELECT * from `users` WHERE userId = '" + req.params.userId + "'";
     connection.query(query, function (err, results) {
         if(err) throw err;
 
-        var result = results[0];
-        console.log(result.userId + "_" + result.username + "_" + result.email + "_" + result.password);
-    })
+        var result = {
+            userId: results[0].userId,
+            username: results[0].username,
+            email: results[0].email,
+            password: results[0].password
+        };
+
+        res.render("profile", { result });
+    });
 });
 
+app.post("/update/:userId", function (req, res) {
+    var post = {
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    // insert to user table
+    var sqlQuery = "UPDATE users SET ? WHERE userId = '" + req.params.userId + "'";
+    query = connection.query(sqlQuery, post, function (err) {
+        if (err) throw err;
+
+        res.redirect("/user");
+    });
+}); */
+
 /* USER interface */
+/*
 app.get("/user", function (req, res) {
     res.render("index");
 });
-
+*/
 /* ADMIN */
+/*
 app.get("/admin", function (req, res) {
 
 });
-
+*/
 // server
 app.listen(3000, function() {
     console.log("Server listen to port 3000");
