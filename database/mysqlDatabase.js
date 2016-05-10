91a2ebef644afe7388493ae3fbad149ee2a6cdc1
Nghi <thanhnghi94@gmail.com>
2016-05-10 21:51:27 +0200

    Add connect module to use in routes folder

---
+++ database/mysqlDatabase.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+
+// connect to database section
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "logInOut"
+});
+
+module.exports = connection.connect(function (err) {
+    if (err) throw err;
+});
