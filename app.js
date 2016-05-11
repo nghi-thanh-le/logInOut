e37fb447e13da57ecfa013eee6edbf36a6b53138
Nghi <thanhnghi94@gmail.com>
2016-05-11 09:48:29 +0200

    Some minor change

--- app.js
+++ app.js
@@ -10,21 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 // app.VERB section
 app.use("/", routes);
-//app.use("/update", update);
-
-/* USER interface */
-/*
-app.get("/user", function (req, res) {
-    res.render("index");
-});
-*/
-/* ADMIN */
-/*
-app.get("/admin", function (req, res) {
-
-});
-*/
-
 // server
 app.listen(3000, function() {
     console.log("Server listen to port 3000");
